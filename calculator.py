d9cf2cf0afbe95fa30cf32515ffe46c356d0e4bb
yaye haile <aryamhaile@gmail.com>
2020-12-25 22:41:18 -0500

    create button

--- calculator.py
+++ calculator.py
@@ -3,3 +3,9 @@ from tkinter import *
 
 #create window
 window = Tk()
+
+# create button for 1-9 and '+', '-', '*', '/', '=', '.', 'clear'
+btn1 = Button(window, text = ' 1 ', font=('arial', 20, 'bold'), bd=5, width=6, height=1, command=lambda : press(1))
+btn1.grid(row=2, column=0)
+
+window.mainloop()
